121061a909d7066894d94f6fa77bac915022f309
MustafaMwafy <mwafy092@gmail.com>
2022-07-26 10:40:44 +0200

    restructure folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-import './assets/main.css'
+import App from './App.vue';
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
