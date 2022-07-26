94a0e3011c73320be39df217fe4777282c6ab9f1
MustafaMwafy <mwafy092@gmail.com>
2022-07-26 13:17:44 +0200

    add most read section

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
     <div class="container">
         <NewsFeeds />
         <NewsForYou />
+        <MostRead />
     </div>
 </template>
 
 <script>
 import NewsFeeds from './components/NewsFeeds.vue';
 import NewsForYou from './components/NewsForYou.vue';
+import MostRead from './components/MostRead.vue';
 export default {
     name: 'App',
     components: {
         NewsFeeds,
         NewsForYou,
+        MostRead,
     },
 };
 </script>
---
+++ src/components/MostRead.vue
@@ -0,0 +1,216 @@
+<template>
+    <div class="section__title">
+        <span class="section__title__border"></span>
+        الاكثر قراءة
+    </div>
+    <div class="row g-4">
+        <div class="col-12 col-lg-6">
+            <div class="news__for__you">
+                <div class="news__for__you__text">
+                    <div class="news__for__you__text__title">
+                        <div class="news__for__you__title__source">
+                            <div class="news__for__you__img">
+                                <img src="../assets/ahramlogo.png" alt="" />
+                            </div>
+                            <p>بوابة الأهرام</p>
+                        </div>
+                        <img
+                            src="../assets/dotted-gray.jpeg"
+                            alt=""
+                            class="dotted__icon"
+                        />
+                    </div>
+                    <div class="news__for__you__main__text">
+                        موعد مباراة بيراميدز وزاناكو في الكونفدرالية الإفريقية
+                        والقناة الناقلة
+                    </div>
+                    <div class="news__for__you__footer">
+                        <div class="news__for__you__date">
+                            <span class="news__for__you__news__source"
+                                >أخبار مصر</span
+                            >
+                            .
+                            <span class="news__for__you__timing"
+                                >منذ ساعتين</span
+                            >
+                        </div>
+                        <div class="news__for__you__controller">
+                            <p>292</p>
+                            <img src="../assets/like-gray.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="news__for__you__image">
+                    <img src="../assets/news.jpeg" />
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6">
+            <div class="row g-4">
+                <div class="news__for__you news__for__you--h col-12">
+                    <div class="news__for__you__text">
+                        <div class="news__for__you__text__title">
+                            <div class="news__for__you__title__source">
+                                <div class="news__for__you__img">
+                                    <img src="../assets/ahramlogo.png" alt="" />
+                                </div>
+                                <p>بوابة الأهرام</p>
+                            </div>
+                            <img
+                                src="../assets/dotted-gray.jpeg"
+                                alt=""
+                                class="dotted__icon"
+                            />
+                        </div>
+                        <div class="news__for__you__main__text">
+                            موعد مباراة بيراميدز وزاناكو في الكونفدرالية
+                            الإفريقية والقناة الناقلة
+                        </div>
+                        <div class="news__for__you__footer">
+                            <div class="news__for__you__date">
+                                <span class="news__for__you__news__source"
+                                    >أخبار مصر</span
+                                >
+                                .
+                                <span class="news__for__you__timing"
+                                    >منذ ساعتين</span
+                                >
+                            </div>
+                            <div class="news__for__you__controller">
+                                <p>292</p>
+                                <img src="../assets/like-gray.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__for__you__image">
+                        <img src="../assets/news.jpeg" />
+                    </div>
+                </div>
+                <div class="news__for__you news__for__you--h col-12">
+                    <div class="news__for__you__text">
+                        <div class="news__for__you__text__title">
+                            <div class="news__for__you__title__source">
+                                <div class="news__for__you__img">
+                                    <img src="../assets/ahramlogo.png" alt="" />
+                                </div>
+                                <p>بوابة الأهرام</p>
+                            </div>
+                            <img
+                                src="../assets/dotted-gray.jpeg"
+                                alt=""
+                                class="dotted__icon"
+                            />
+                        </div>
+                        <div class="news__for__you__main__text">
+                            موعد مباراة بيراميدز وزاناكو في الكونفدرالية
+                            الإفريقية والقناة الناقلة
+                        </div>
+                        <div class="news__for__you__footer">
+                            <div class="news__for__you__date">
+                                <span class="news__for__you__news__source"
+                                    >أخبار مصر</span
+                                >
+                                .
+                                <span class="news__for__you__timing"
+                                    >منذ ساعتين</span
+                                >
+                            </div>
+                            <div class="news__for__you__controller">
+                                <p>292</p>
+                                <img src="../assets/like-gray.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__for__you__image">
+                        <img src="../assets/news.jpeg" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="news__for__you news__for__you--h col-12 col-lg-6">
+            <div class="news__for__you__text">
+                <div class="news__for__you__text__title">
+                    <div class="news__for__you__title__source">
+                        <div class="news__for__you__img">
+                            <img src="../assets/ahramlogo.png" alt="" />
+                        </div>
+                        <p>بوابة الأهرام</p>
+                    </div>
+                    <img
+                        src="../assets/dotted-gray.jpeg"
+                        alt=""
+                        class="dotted__icon"
+                    />
+                </div>
+                <div class="news__for__you__main__text">
+                    موعد مباراة بيراميدز وزاناكو في الكونفدرالية الإفريقية
+                    والقناة الناقلة
+                </div>
+                <div class="news__for__you__footer">
+                    <div class="news__for__you__date">
+                        <span class="news__for__you__news__source"
+                            >أخبار مصر</span
+                        >
+                        .
+                        <span class="news__for__you__timing">منذ ساعتين</span>
+                    </div>
+                    <div class="news__for__you__controller">
+                        <p>292</p>
+                        <img src="../assets/like-gray.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="news__for__you__image">
+                <img src="../assets/news.jpeg" />
+            </div>
+        </div>
+        <div class="news__for__you news__for__you--h col-12 col-lg-6">
+            <div class="news__for__you__text">
+                <div class="news__for__you__text__title">
+                    <div class="news__for__you__title__source">
+                        <div class="news__for__you__img">
+                            <img src="../assets/ahramlogo.png" alt="" />
+                        </div>
+                        <p>بوابة الأهرام</p>
+                    </div>
+                    <img
+                        src="../assets/dotted-gray.jpeg"
+                        alt=""
+                        class="dotted__icon"
+                    />
+                </div>
+                <div class="news__for__you__main__text">
+                    موعد مباراة بيراميدز وزاناكو في الكونفدرالية الإفريقية
+                    والقناة الناقلة
+                </div>
+                <div class="news__for__you__footer">
+                    <div class="news__for__you__date">
+                        <span class="news__for__you__news__source"
+                            >أخبار مصر</span
+                        >
+                        .
+                        <span class="news__for__you__timing">منذ ساعتين</span>
+                    </div>
+                    <div class="news__for__you__controller">
+                        <p>292</p>
+                        <img src="../assets/like-gray.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="news__for__you__image">
+                <img src="../assets/news.jpeg" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MostRead',
+};
+</script>
+
+<style lang="scss">
+@import '../styles/most-read.scss';
+</style>
