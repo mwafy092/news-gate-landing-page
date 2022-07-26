3ecf36f4034751219b329bd1902de302a6481c1e
MustafaMwafy <mwafy092@gmail.com>
2022-07-26 11:49:06 +0200

    adding news feeds section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>Hello</div>
+    <div class="container">
+        <NewsFeeds />
+    </div>
 </template>
 
 <script>
+import NewsFeeds from './components/NewsFeeds.vue';
 export default {
     name: 'App',
+    components: {
+        NewsFeeds,
+    },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+body {
+    background-color: #f4f4f4;
+}
+</style>
---
+++ src/components/NewsFeeds.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="row news__feeds g-2">
+        <div class="col-12 col-md-12 col-lg-8">
+            <div class="news__feeds__main">
+                <img src="../assets/news.jpeg" />
+                <div class="news__details">
+                    <div class="news__details__title">
+                        <div class="news__details__title__info">
+                            <div class="news__details__title__source">
+                                <img src="../assets/ahramlogo.png" alt="" />
+                            </div>
+                            <p>بوابة الأهرام</p>
+                        </div>
+                        <div class="news__details__title__controllers">
+                            <p>292</p>
+                            <img src="../assets/like.png" alt="" />
+                            <img src="../assets/dotted.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="news__details__info">
+                        <p class="news__details__info__main">
+                            الرئيس السيسي يوجه بالمحافظة على استمرارية المخزون
+                            الإستراتيجي للدولة من السلع الغذائية
+                        </p>
+                        <div class="news__details__info__date">
+                            <span>أخبار مصر</span>
+                            .
+                            <span>منذ ساعتين</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-12 col-lg-4 news__feeds__latest">
+            <div class="row g-2 g-lg-1">
+                <div class="news__feeds__main col-md-6 col-lg-12">
+                    <img src="../assets/news.jpeg" />
+                    <div class="news__details">
+                        <div class="news__details__title">
+                            <div class="news__details__title__info">
+                                <div class="news__details__title__source">
+                                    <img src="../assets/ahramlogo.png" alt="" />
+                                </div>
+                                <p>بوابة الأهرام</p>
+                            </div>
+                            <div class="news__details__title__controllers">
+                                <p>292</p>
+                                <img src="../assets/like.png" alt="" />
+                                <img src="../assets/dotted.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="news__details__info">
+                            <p class="news__details__info__main">
+                                الرئيس السيسي يوجه بالمحافظة على استمرارية
+                                المخزون الإستراتيجي للدولة من السلع الغذائية
+                            </p>
+                            <div class="news__details__info__date">
+                                <span>أخبار مصر</span>
+                                .
+                                <span>منذ ساعتين</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="news__feeds__main col-md-6 col-lg-12">
+                    <img src="../assets/news.jpeg" />
+                    <div class="news__details">
+                        <div class="news__details__title">
+                            <div class="news__details__title__info">
+                                <div class="news__details__title__source">
+                                    <img src="../assets/ahramlogo.png" alt="" />
+                                </div>
+                                <p>بوابة الأهرام</p>
+                            </div>
+                            <div class="news__details__title__controllers">
+                                <p>292</p>
+                                <img src="../assets/like.png" alt="" />
+                                <img src="../assets/dotted.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="news__details__info">
+                            <p class="news__details__info__main">
+                                الرئيس السيسي يوجه بالمحافظة على استمرارية
+                                المخزون الإستراتيجي للدولة من السلع الغذائية
+                            </p>
+                            <div class="news__details__info__date">
+                                <span>أخبار مصر</span>
+                                .
+                                <span>منذ ساعتين</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewsFeeds',
+};
+</script>
+
+<style lang="scss">
+@import '../styles/news-feeds.scss';
+</style>
