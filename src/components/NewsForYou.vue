3beb701574f961ee73d15e9107b8c6a929c0d95e
MustafaMwafy <mwafy092@gmail.com>
2022-08-01 13:26:42 +0200

    fix styles after adding generic items

--- src/components/NewsForYou.vue
+++ src/components/NewsForYou.vue
@@ -1,31 +1,27 @@
 <template>
-    <div class="row my-5 g-4">
-        <div class="section__title">
-            <span class="section__title__border"></span>
-            اخبار من اجلك
-        </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
+    <div class="section__title">
+        <span class="section__title__border"></span>
+        اخبار من اجلك
+    </div>
+    <div class="row g-4">
+        <div class="col-md-6 col-xl-4">
             <NewsGeneric />
         </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
+        <div class="col-md-6 col-xl-4">
             <NewsGeneric />
         </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
-            <div class="row g-4">
-                <NewsGeneric position="h" />
-                <NewsGeneric position="h" />
-            </div>
+        <div class="col-md-6 col-xl-4 d-grid gap-4">
+            <NewsGeneric position="h" />
+            <NewsGeneric position="h" />
         </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
-            <div class="row g-4">
-                <NewsGeneric position="h" />
-                <NewsGeneric position="h" />
-            </div>
+        <div class="col-md-6 col-xl-4 d-grid gap-4">
+            <NewsGeneric position="h" />
+            <NewsGeneric position="h" />
         </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
+        <div class="col-md-6 col-xl-4">
             <NewsGeneric />
         </div>
-        <div class="col-12 col-lg-6 col-xxl-4">
+        <div class="col-md-6 col-xl-4">
             <NewsGeneric />
         </div>
     </div>
